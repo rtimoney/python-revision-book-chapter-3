153c717424a1b1e80bb0fc1a7725fc9dd97170ac
rtimoney <ronan.timoney@gmail.com>
2023-02-11 17:03:39 +0000

    update 11th Feb

---
+++ order.py
@@ -0,0 +1,21 @@
+# declare and initialize example variables for demonstartion purposes
+a = 2
+b = 4
+c = 8
+
+# addition and multiplication
+print ('\nDefault Order:\t', a ,'*' ,c,'+',b,'=',a*c+b)
+print ('Forced Order:\t', a ,'* (' ,c,'+',b,') =',a*(c+b))
+
+# subtraction and division
+print ('\nDefault Order:\t', c ,'//' ,b,'-',a,'=',c//b-a)
+print ('Default Order:\t', c ,'// (' ,b,'-',a,') =',c//(b-a))
+
+# addition and remainder 
+print ('\nDefault Order:\t', c ,'%' ,a,'+',b,'=',c%a+b)
+print ('Default Order:\t', c ,'%' ,a,'+',b,'=',c%(a+b))
+
+
+# addition and exponential (**)
+print ('\nDefault Order:\t', c ,'**' ,a,'+',b,'=',c**a+b)
+print ('Default Order:\t', c ,'**' ,a,'+',b,'=',c**(a+b))
